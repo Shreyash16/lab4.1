b7165130dbfa88d8d01e19802c7ef14e487f8fe4
Shreyash16 <32540618+Shreyash16@users.noreply.github.com>
2020-04-30 22:55:20 +0530

    Add files via upload

---
+++ Lab4.1(assignment)/src/main/java/shreyash/cg/gla/spring/dao/ProductDaoInterface.java
@@ -0,0 +1,14 @@
+package shreyash.cg.gla.spring.dao;
+
+import java.util.List;
+
+import shreyash.cg.gla.spring.entity.Product;
+
+
+public interface ProductDaoInterface {
+	
+	public boolean create(Product product);
+	public List<Product> reterive();
+	
+
+}
